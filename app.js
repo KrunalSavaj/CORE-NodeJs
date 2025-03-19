8c89ac1e68dcd9d50a83f538fb4bd800de52754d
KrunalSavaj <developer.krunalsavaj@gmail.com>
2025-03-19 11:32:04 +0530

    implement templates view in use of pug, ejs,hbs

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const path = require("path");
-const rootDir = require('./util/path')
-const bodyParser = require("body-parser");
+const path = require('path');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use("/admin", adminRoutes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '*' });
 });
 
 app.listen(3000);
+
