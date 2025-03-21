bcfa8be93fe47e264ee2e075ffd8caa0fae2c388
developer.krunalsavaj@gmail.com <developer.krunalsavaj@gmail.com>
2025-03-21 16:47:55 +0530

    implement mysql database

--- models/product.js
+++ models/product.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
+const db = require('../util/database')
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
     this.title = title;
@@ -26,15 +8,20 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+    db.execute('INSERT INTO products (title, price, imageUrl, description) VALUSE (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    )
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static deleteById(id) {
+
+  }
+
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
