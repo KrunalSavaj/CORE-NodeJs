480e2bdcb30e167aab9d649ff5b343519f3df603
KrunalSavaj <developer.krunalsavaj@gmail.com>
2025-03-25 17:01:17 +0530

    add order and cart modal and implement their routes as well

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,6 +39,13 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({force: true})
@@ -49,7 +60,8 @@ sequelize
     return user;
   })
   .then(user => {
-    // console.log(user);
+    user.createCart();
+  }).then(cart => {
     app.listen(3000);
   }).catch(err => {
     console.log(err);
