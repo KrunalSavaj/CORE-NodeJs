5ebc20c66cf76765eff96473dd65db41282f6750
KrunalSavaj <developer.krunalsavaj@gmail.com>
2025-03-26 15:15:14 +0530

    fix code format

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const errorController = require("./controllers/error");
+
 const sequelize = require('./util/database');
+const errorController = require("./controllers/error");
+
+const Cart = require("./models/cart");
 const User = require('./models/user');
+const Order = require("./models/order");
 const Product = require('./models/product');
+const CartItem = require("./models/cart-item");
+const OrderItem = require("./models/order-item");
+
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
